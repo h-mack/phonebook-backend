1577a46f090a8b42e5cbf958521721e00a44c38c
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-04-03 15:54:25 +1000

    3.1 phonebook backend setup

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+const app = express();
+const port = 3001;
+
+const data = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send(`
+    <a href="/api">api list</a>
+    `);
+});
+
+app.get("/api", (req, res) => {
+  res.send(`<a href="/api/persons">Persons</a>`);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
