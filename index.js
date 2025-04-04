57c832a33e53df2a81bbcc59600f263ee9bbd5f2
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-04-04 13:25:05 +1000

    3.4 single resource delete request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { homepage, apiList, info } from "./pages/index.js";
-import { data } from "./data.js";
+import data from "./data.js";
+
+let persons = data.persons;
 
 const app = express();
 const port = 3001;
@@ -14,17 +16,24 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.send(data);
+  res.send(persons);
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = data.find((p) => p.id === id);
+  const person = persons.find((p) => p.id === id);
   if (person) {
     res.send(person);
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
--- pages/info.js
+++ pages/info.js
@@ -1,10 +1,11 @@
-import { data } from "../data.js";
+import data from "../data.js";
+let persons = data.persons;
 
 let currentDate = new Date();
 
 export const info = `
 <div>
-  <p>Phonebook has info for ${data.length} people</p>
+  <p>Phonebook has info for ${persons.length} people</p>
   </br>
   <p>${currentDate.toString()}</p>
 </div>
