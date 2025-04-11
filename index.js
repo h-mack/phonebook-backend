31ce3c309904e46f5be8b9466ea4fc89d04187b9
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-04-11 17:42:25 +1000

    3.8 configure morgan to show data sent in HTTP POST requests

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ let persons = data.persons;
 const app = express();
 const port = 3001;
 
+morgan.token("content", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["content"](req, res),
+    ].join(" ");
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(homepage);
