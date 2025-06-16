79fff276ce5b24fa7d8c672c5f848efbd9bf82ca
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-06-16 15:28:02 +1000

    add unknown endpoint and error handlers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import { apiList, info } from "./pages/index.js";
-import { Contact } from './models/contact.js'
-import mongoose from 'mongoose';
+import { Contact } from "./models/contact.js";
+import mongoose from "mongoose";
 
 const port = process.env.PORT;
 const app = express();
@@ -12,6 +12,7 @@ morgan.token("content", function (req, res) {
   return JSON.stringify(req.body);
 });
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan((tokens, req, res) => {
@@ -27,31 +28,29 @@ app.use(
     ].join(" ");
   })
 );
-app.use(express.static('dist'));
 
 app.get("/api", (req, res) => {
   res.send(apiList);
 });
 
 app.get("/api/persons", (req, res) => {
-    Contact.find({}).then(contacts => {
+  Contact.find({}).then((contacts) => {
     res.json(contacts);
-  })
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
 
-  Contact.findById(id).then(contact => {
-    if (contact) {
-      res.json(contact)
-    } else {
-      res.status(404).end();
-    }
-  }).catch(error => {
-    console.error(error);
-    res.status(400).send({ error: 'malformatted id'});
-  })
+  Contact.findById(id)
+    .then((contact) => {
+      if (contact) {
+        res.json(contact);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", async (req, res) => {
@@ -61,7 +60,7 @@ app.delete("/api/persons/:id", async (req, res) => {
   const result = await Contact.deleteOne({ _id: objectId });
 
   if (result.deletedCount === 0) {
-    return res.status(404).json({ error: "Contact not found"});
+    return res.status(404).json({ error: "Contact not found" });
   }
 
   res.status(204).end();
@@ -87,13 +86,16 @@ app.post("/api/persons", (req, res) => {
     number: body.number,
   });
 
-  contact.save().then(result => {
-    console.log('contact saved');
-    res.json(result);
-  }).catch(error => {
-    console.error(error);
-    res.status(400);
-  })
+  contact
+    .save()
+    .then((result) => {
+      console.log("contact saved");
+      res.json(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400);
+    });
 
   // if (persons.some((person) => person.name === body.name)) {
   //   return res.status(400).json({
@@ -107,6 +109,24 @@ app.post("/api/persons", (req, res) => {
 app.get("/info", (req, res) => {
   res.send(info);
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
