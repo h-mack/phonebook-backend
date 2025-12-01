d671667d23189344de9ed8ed0012a4bed8a6ad4e
h-mack <89110856+h-mack@users.noreply.github.com>
2025-12-01 05:35:17 +0000

    change /api/persons to use controller route handler

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
 import express from "express";
-import mongoose from "mongoose";
 import morgan from "morgan";
-import { apiList, info } from "./pages/index";
-import { Contact } from "./models/contact";
+import { apiList } from "./pages/index";
 import { errorHandler, unknownEndpoint } from "./utils/middleware";
+import contactsRouter from "./controllers/contacts";
 
 const app = express();
 
@@ -33,83 +32,7 @@ app.get("/api", (_req, res) => {
   res.send(apiList);
 });
 
-app.get("/api/persons", (_req, res) => {
-  Contact.find({}).then((contacts) => {
-    res.json(contacts);
-  });
-});
-
-app.get("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
-
-  Contact.findById(id)
-    .then((contact) => {
-      if (contact) {
-        res.json(contact);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.delete("/api/persons/:id", async (req, res) => {
-  const id = req.params.id;
-  const objectId = new mongoose.Types.ObjectId(id);
-
-  const result = await Contact.deleteOne({ _id: objectId });
-
-  if (result.deletedCount === 0) {
-    return res.status(404).json({ error: "Contact not found" });
-  }
-
-  res.status(204).end();
-});
-
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-
-  if (!body.name) {
-    return res.status(400).json({
-      error: "name missing",
-    });
-  }
-
-  if (!body.number) {
-    return res.status(400).json({
-      error: "number missing",
-    });
-  }
-
-  const contact = new Contact({
-    name: body.name,
-    number: body.number,
-  });
-
-  contact
-    .save()
-    .then((result) => {
-      console.log("contact saved");
-      res.json(result);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(400);
-    });
-
-  // if (persons.some((person) => person.name === body.name)) {
-  //   return res.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
-
-  // res.json(person);
-});
-
-app.get("/info", (_req, res) => {
-  res.send(info);
-});
-
+app.use("/api/persons", contactsRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
