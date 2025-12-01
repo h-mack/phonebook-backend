4ad1c566f7700e288067a62a33c044350c697fc4
h-mack <89110856+h-mack@users.noreply.github.com>
2025-12-01 21:28:35 +0000

    fix build error

--- models/contact.ts
+++ models/contact.ts
@@ -25,12 +25,4 @@ const contactSchema = new mongoose.Schema({
   number: String,
 });
 
-contactSchema.set("toJSON", {
-  transform: (_document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
 export const Contact = mongoose.model("Contact", contactSchema);
